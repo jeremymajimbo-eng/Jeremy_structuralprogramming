6e47626cf66a41465816d4dc932d787aa49f557d
jeremymajimbo-eng <jeremy.majimbo@students.jkuat.ac.ke>
2025-11-19 23:56:32 +0300

    Add files via upload

--- Assignment1/main.c
+++ Assignment1/main.c
@@ -1,30 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>   // <-- REQUIRED for strlen()
 
-int main() {
-    int num1, num2;
+int main()
+{
+    char name[100];
 
-    // Input from user
-    printf("Enter first number: ");
-    scanf("%d", &num1);
+    printf("Enter a string: ");
+    scanf("%s", name);
 
-    printf("Enter second number: ");
-    scanf("%d", &num2);
+    printf("You entered: %s\n", name);
 
-    // Performing operations
-    printf("\n--- Results ---\n");
-    printf("Addition: %d\n", num1 + num2);
-    printf("Subtraction: %d\n", num1 - num2);
-    printf("Multiplication: %d\n", num1 * num2);
+    int length = strlen(name);
 
-    // Handling division and modulus safely
-    if (num2 != 0) {
-        printf("Division: %d\n", num1 / num2);
-        printf("Modulus: %d\n", num1 % num2);
-    } else {
-        printf("Division: Undefined (division by zero)\n");
-        printf("Modulus: Undefined (modulus by zero)\n");
-    }
+    printf("Length of the string: %d\n", length);
 
     return 0;
 }
+

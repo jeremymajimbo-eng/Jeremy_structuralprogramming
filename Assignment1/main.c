e3d49a071b2b931e879606b0571fbccaf6d7524c
jeremymajimbo-eng <jeremy.majimbo@students.jkuat.ac.ke>
2025-11-19 23:59:57 +0300

    Add files via upload

--- Assignment1/main.c
+++ Assignment1/main.c
@@ -1,19 +1,52 @@
 #include <stdio.h>
-#include <string.h>   // <-- REQUIRED for strlen()
+#include <stdlib.h>
 
 int main()
 {
-    char name[100];
+    int x = 1234;
+    int y ;
+    int choice;
 
-    printf("Enter a string: ");
-    scanf("%s", name);
 
-    printf("You entered: %s\n", name);
+    printf("enter pin: ");
+    scanf("%d", &y);
 
-    int length = strlen(name);
+     if (x==y){
+        printf("Access granted\n");
 
-    printf("Length of the string: %d\n", length);
+        printf("Select an action below:\n");
+        printf("1 - Open door\n");
+        printf("2 - Change username\n");
+        printf("3 - Change Pin\n");
+        printf("4 - Exit\n");
+        printf("Enter your choice:\n");
+        scanf("%d", &choice);
 
-    return 0;
+
+    switch (choice) {
+    case 1:
+        printf( "Door is opening...");
+        break;
+
+    case 2:
+        printf("Username change selected");
+        break;
+
+    case 3:
+        printf("Pin change selected");
+        break;
+
+    case 4:
+        printf("Existing system");
+        break;
+
+    default:
+        printf("Invalid option");
+    }
+
+    }else {
+       printf("Wrong pin! Access denied");
+
+    }
+          return 0;
 }
-
